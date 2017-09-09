5d73ea9daf8ad792b25f465fd363038014c7760e
Tisks <leiser.mahu@gmail.com>
2017-09-08 21:30:03 -0300

    Vue

---
+++ tbd-2-2017-vue/plantilla-vue/src/ActorsCreate.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="center">
+
+      <h1>Crear Actor</h1>
+
+      <div>
+        <h3>Nombre</h3>
+        <input type="text" name="nombre" v-model="actor.firstName" required>
+      </div>
+      <div>
+        <h3>Apellido</h3>
+        <input type="text" name="Apellido"  v-model="actor.lastName" required>
+      </div>
+
+
+      <button v-on:click.prevent="post" type="submit" name="agregar"> Aceptar </button>
+
+
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  data(){
+    return{
+      actor:{
+        firstName:'',
+        lastName:''
+      }
+    }
+  },
+  methods: {
+    post:function(){
+      var actor = this.actor
+      this.actor = {firstName:'' , lastName:''}
+      this.$http.post('http://localhost:8081/sakila-spring-backend/actors', actor
+      ).then(function(data){
+        console.log(data);
+      },response => {
+    // error callback
+      });
+    }
+  }
+}
+
+
+
+</script>
+
